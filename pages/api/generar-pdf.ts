037f1ad23ea22228bfa79dc43cf45be744940e3a
Gojosatoruinfinito <josejesussanchez2@gmail.com>
2025-07-29 18:40:23 -0400

    Trigger redeploy

--- pages/api/generar-pdf.ts
+++ pages/api/generar-pdf.ts
@@ -11,6 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
      return res.status(405).json({ error: 'Método no permitido' });
    }
 
+   
     const products = req.body.products;
 
     if (!Array.isArray(products)) {
