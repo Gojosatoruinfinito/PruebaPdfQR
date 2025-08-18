bbcb8332495bc47132787db8216bff2e22a27eb0
Gojosatoruinfinito <josejesussanchez2@gmail.com>
2025-08-18 18:16:11 -0400

    Ajustes api Eliminar

--- pages/api/DeletePdf.ts
+++ pages/api/DeletePdf.ts
@@ -17,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // Respuesta en JSON
       return res.status(200).json({ success: true, message: "Factura eliminada con éxito" });
     } catch (error: any) {
-      return res.status(500).json({ success: req.body, error:  req.body });
+      return res.status(500).json({ success: false, error: error.message });
     }
   } else {
     res.status(405).json({ success: false, error: "Método no permitido" });
